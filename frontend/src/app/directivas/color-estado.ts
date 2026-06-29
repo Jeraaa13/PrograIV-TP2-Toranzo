1a00581921c29b53727d6919958b741dbf7fdb41
Jeraaa13 <gerardo.m.toranzo@gmail.com>
2026-06-29 14:48:42 -0300

    Boton a estadisticas + pipes

--- frontend/src/app/directivas/color-estado.ts
+++ frontend/src/app/directivas/color-estado.ts
@@ -1,14 +1,14 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[appColorEstado]',
 })
-export class ColorEstado implements OnInit {
+export class ColorEstado implements OnChanges {
   @Input() appColorEstado!: boolean;
 
   constructor(private el: ElementRef) {}
 
-  ngOnInit(): void {
+  ngOnChanges() {
     if (this.appColorEstado) {
       this.el.nativeElement.style.color = '#e74c3c';
     } else {
